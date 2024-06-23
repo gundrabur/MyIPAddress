e4ce2fa9a2a133b104dac463a408d8a969c38581
Chris Moeller <38981012+gundrabur@users.noreply.github.com>
2024-06-23 12:18:53 +0200

    no loop anymore

--- myipaddress.sh
+++ myipaddress.sh
@@ -20,34 +20,27 @@ fi
 # Initialize a variable to store the previously recorded IP address
 prev_ip_address=""
 
-# Infinite loop to continuously monitor IP address changes
-while true; do
-    # Get the current date and time in YYYY-MM-DD HH:MM:SS format for timestamp
-    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+# Get the current date and time in YYYY-MM-DD HH:MM:SS format for timestamp
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-    # Use 'ifconfig' to get network interface information
-    # Filter loopback addresses (127.0.0.1) and extract the IPv4 address using awk
-    ip_address=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
+# Use 'ifconfig' to get network interface information
+# Filter loopback addresses (127.0.0.1) and extract the IPv4 address using awk
+ip_address=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
 
-    # Check if the current IP address is different from the previously recorded one
-    if [ "$ip_address" != "$prev_ip_address" ]; then
-        # Display timestamp and IP address on the terminal with a newline
-        echo -e "[$timestamp] $ip_address\n"
+# Check if the current IP address is different from the previously recorded one
+if [ "$ip_address" != "$prev_ip_address" ]; then
+    # Display timestamp and IP address on the terminal with a newline
+    echo -e "[$timestamp] $ip_address\n"
 
-        # Write the timestamp and IP address to the output file, overwriting previous content
-        echo "[$timestamp] $ip_address" > "$output_file"
+    # Write the timestamp and IP address to the output file, overwriting previous content
+    echo "[$timestamp] $ip_address" > "$output_file"
 
-        # Update the previous IP address variable for comparison in the next loop iteration
-        prev_ip_address="$ip_address"
+    # Update the previous IP address variable for comparison in the next loop iteration
+    prev_ip_address="$ip_address"
 
-    else
-        # Display the timestamp and IP address in the terminal
-        echo -e "[$timestamp] no IP change $ip_address\n$output_file"
-        # Write the timestamp and IP address to the specified text file
-        echo "[$timestamp] no IP change $ip_address" > "$output_file" 
-    fi
-
-    # Pause the script for 60 seconds before checking the IP address again
-    # no need any more, since we're running this script from launchd
-    # sleep 60
-done
+else
+    # Display the timestamp and IP address in the terminal
+    echo -e "[$timestamp] no IP change $ip_address\n$output_file"
+    # Write the timestamp and IP address to the specified text file
+    echo "[$timestamp] no IP change $ip_address" > "$output_file" 
+fi
